7941e444ec97cbde8633e21ab2c84d25315efcb0
artemkiruhin <temkaunik@gmail.com>
2025-06-28 21:36:31 +0300

    add: factory method for task entity

--- backend/LifeTrack.Core/Models/Entities/KanbanTaskEntity.cs
+++ backend/LifeTrack.Core/Models/Entities/KanbanTaskEntity.cs
@@ -7,11 +7,25 @@ public class KanbanTaskEntity
     public string? DescriptionMarkdown { get; set; }
     public bool IsImportant { get; set; }
     public bool IsCompleted { get; set; }
-    public Guid TaskCategoryId { get; set; }
+    public Guid? TaskCategoryId { get; set; }
     public Guid UserId { get; set; }
     public DateTime CreatedAt { get; set; }
-    public DateTime UpdatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
     
     public virtual KanbanTaskCategoryEntity Category { get; set; } = null!;
     public virtual UserEntity User { get; set; } = null!;
+
+    public static KanbanTaskEntity Create(string title, string? descriptionMarkdown, bool isImportant, Guid? taskCategoryId, Guid userId) =>
+        new()
+        {
+            Id = Guid.NewGuid(),
+            Title = title,
+            DescriptionMarkdown = descriptionMarkdown,
+            IsImportant = isImportant,
+            IsCompleted = false,
+            TaskCategoryId = taskCategoryId,
+            UserId = userId,
+            CreatedAt = DateTime.UtcNow,
+            UpdatedAt = null
+        };
 }
