682b64777e6d4d31515e4b4551a0403bf1c6e562
artemkiruhin <temkaunik@gmail.com>
2025-06-28 21:25:16 +0300

    add: user entity

---
+++ backend/LifeTrack.Core/Models/Entities/UserEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace LifeTrack.Core.Models.Entities;
+
+public class UserEntity
+{
+    public Guid Id { get; set; }
+    public required string Username { get; set; }
+    public required string PasswordHash { get; set; }
+    public required string Email { get; set; }
+    public required string SecurityPin { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
