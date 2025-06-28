32e10ffa0fb59d8164d07a5127cd55adec8be963
artemkiruhin <temkaunik@gmail.com>
2025-06-28 21:30:15 +0300

    add: task category

---
+++ backend/LifeTrack.Core/Models/Entities/KanbanTaskCategoryEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace LifeTrack.Core.Models.Entities;
+
+public class KanbanTaskCategoryEntity
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
