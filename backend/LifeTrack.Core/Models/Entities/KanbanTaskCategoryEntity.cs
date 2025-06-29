b27c57d50c62c56a67a974ff04ff6a6220f35a71
artemkiruhin <temkaunik@gmail.com>
2025-06-29 21:44:48 +0300

    add: user ref for kanban category

--- backend/LifeTrack.Core/Models/Entities/KanbanTaskCategoryEntity.cs
+++ backend/LifeTrack.Core/Models/Entities/KanbanTaskCategoryEntity.cs
@@ -5,14 +5,17 @@ public class KanbanTaskCategoryEntity
     public Guid Id { get; set; }
     public required string Name { get; set; }
     public DateTime CreatedAt { get; set; }
+    public Guid UserId { get; set; }
 
     public virtual ICollection<KanbanTaskEntity> Tasks { get; set; } = [];
+    public virtual UserEntity User { get; set; } = null!;
 
-    public static KanbanTaskCategoryEntity Create(string name) =>
+    public static KanbanTaskCategoryEntity Create(string name, Guid userId) =>
         new()
         {
             Id = Guid.NewGuid(),
             Name = name,
+            UserId = userId,
             CreatedAt = DateTime.UtcNow
         };
 }
