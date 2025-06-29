939694d493ecdb25ad347075f57703caf3f4c318
artemkiruhin <temkaunik@gmail.com>
2025-06-29 08:06:59 +0300

    add: kanban task create contract

---
+++ backend/LifeTrack.Core/Models/Contracts/Create/KanbanTaskCreateContract.cs
@@ -0,0 +1,3 @@
+﻿namespace LifeTrack.Core.Models.Contracts.Create;
+
+public record KanbanTaskCreateContract(string Title, string? DescriptionMarkdown, bool IsImportant, Guid? TaskCategoryId, Guid UserId);
