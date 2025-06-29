528c17990fa2e3100cf0c8f782b2140ba643e187
artemkiruhin <temkaunik@gmail.com>
2025-06-29 21:11:17 +0300

    add: user repository interface

---
+++ backend/LifeTrack.Core/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using LifeTrack.Core.Interfaces.Repositories.Base;
+using LifeTrack.Core.Models.Entities;
+
+namespace LifeTrack.Core.Interfaces.Repositories;
+
+public interface IUserRepository: IRepository<UserEntity>
+{
+    Task<UserEntity?> GetByEmailAsync(string email, CancellationToken ct);
+    Task<UserEntity?> GetByUsernameAsync(string username, CancellationToken ct);
+}
