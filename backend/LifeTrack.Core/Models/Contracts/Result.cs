ac6cfac48eea774ab6d2ddece131898634580b0d
artemkiruhin <temkaunik@gmail.com>
2025-06-29 21:24:21 +0300

    add: result DTO

---
+++ backend/LifeTrack.Core/Models/Contracts/Result.cs
@@ -0,0 +1,25 @@
+﻿namespace LifeTrack.Core.Models.Contracts;
+
+public class Result<TResult>
+{
+    public string ErrorMessage { get; set; }
+    public TResult? Data { get; set; }
+    public bool IsSuccess { get; set; }
+
+    private Result(TResult? data)
+    {
+        Data = data;
+        IsSuccess = true;
+        ErrorMessage = "";
+    }
+
+    private Result(string errorMessage)
+    {
+        ErrorMessage = errorMessage;
+        IsSuccess = false;
+        Data = default(TResult);
+    }
+
+    public static Result<TResult> Success(TResult data) => new(data: data);
+    public static Result<TResult> Failure(string errorMessage) => new(errorMessage: errorMessage);
+}
