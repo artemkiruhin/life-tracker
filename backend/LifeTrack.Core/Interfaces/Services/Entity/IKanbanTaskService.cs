3b80c3686c94209ed1f538a0cc8f8b8346ebdaa9
artemkiruhin <temkaunik@gmail.com>
2025-06-29 21:12:07 +0300

    add: kanban task service interface

---
+++ backend/LifeTrack.Core/Interfaces/Services/Entity/IKanbanTaskService.cs
@@ -0,0 +1,17 @@
+﻿using LifeTrack.Core.Models.Contracts.Create;
+using LifeTrack.Core.Models.Contracts.Specific;
+using LifeTrack.Core.Models.Contracts.Update;
+using LifeTrack.Core.Models.DTOs;
+
+namespace LifeTrack.Core.Interfaces.Services.Entity;
+
+public interface IKanbanTaskService
+{
+    Task<KanbanTaskDTO> GetTaskById (Guid taskId, Guid userId, CancellationToken ct);
+    Task<List<KanbanTaskDTO>> GetAllCategories (Guid userId, CancellationToken ct);
+    Task<KanbanTaskDTO> Create (KanbanTaskCreateContract request, CancellationToken ct);
+    Task<KanbanTaskDTO> Update (KanbanTaskUpdateContract request, CancellationToken ct);
+    Task<KanbanTaskDTO> Delete (Guid taskId, Guid userId, CancellationToken ct);
+    Task<KanbanTaskDTO> RemoveCategory (RemoveCategoryContract request, CancellationToken ct);
+    Task<KanbanTaskDTO> ChangeCategory (ChangeCategoryContract request, CancellationToken ct);
+}
