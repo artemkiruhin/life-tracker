4550f1aa9e6bbcf9e082201341407c175c9d8f97
artemkiruhin <temkaunik@gmail.com>
2025-06-29 21:11:01 +0300

    add: kanban task repository interface

---
+++ backend/LifeTrack.Core/Interfaces/Repositories/IKanbanTaskRepository.cs
@@ -0,0 +1,9 @@
+﻿using LifeTrack.Core.Interfaces.Repositories.Base;
+using LifeTrack.Core.Models.Entities;
+
+namespace LifeTrack.Core.Interfaces.Repositories;
+
+public interface IKanbanTaskRepository: IRepository<KanbanTaskEntity>
+{
+    
+}
