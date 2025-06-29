c693d0c8ac03d42123d7151bd69fa54d4b1bf5d0
artemkiruhin <temkaunik@gmail.com>
2025-06-29 21:11:48 +0300

    add: user service interface

---
+++ backend/LifeTrack.Core/Interfaces/Services/Entity/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using LifeTrack.Core.Models.Contracts.Create;
+using LifeTrack.Core.Models.Contracts.Specific;
+using LifeTrack.Core.Models.Contracts.Update;
+using LifeTrack.Core.Models.DTOs;
+
+namespace LifeTrack.Core.Interfaces.Services.Entity;
+
+public interface IUserService
+{
+    Task<UserDTO> GetUser(Guid userId, CancellationToken ct);
+    Task<UserDTO> Create (UserCreateContract request, CancellationToken ct);
+    Task<UserDTO> Update (UserUpdateContract request, CancellationToken ct);
+    Task<UserDTO> Delete (Guid userId, CancellationToken ct);
+    Task<UserDTO> ResetPassword (ResetPasswordContract request, CancellationToken ct);
+    Task<UserDTO> ResetPassword (ResetPasswordContractByPin request, CancellationToken ct);
+}
