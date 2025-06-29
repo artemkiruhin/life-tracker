e2c8a470e30c41761f5325550549cceec7d902b0
artemkiruhin <temkaunik@gmail.com>
2025-06-29 21:12:29 +0300

    add: kanban task mapper interface

---
+++ backend/LifeTrack.Core/Interfaces/Services/Mappers/IKanbanTaskMapper.cs
@@ -0,0 +1,9 @@
+﻿using LifeTrack.Core.Models.DTOs;
+using LifeTrack.Core.Models.Entities;
+
+namespace LifeTrack.Core.Interfaces.Services.Mappers;
+
+public interface IKanbanTaskMapper
+{
+    KanbanTaskDTO Map (KanbanTaskEntity entity);
+}
