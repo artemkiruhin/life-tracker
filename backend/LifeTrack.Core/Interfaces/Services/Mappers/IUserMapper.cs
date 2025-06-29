28f7b339b82c778b462e89b95feac739ec5ac266
artemkiruhin <temkaunik@gmail.com>
2025-06-29 21:12:20 +0300

    add: user mapper interface

---
+++ backend/LifeTrack.Core/Interfaces/Services/Mappers/IUserMapper.cs
@@ -0,0 +1,9 @@
+﻿using LifeTrack.Core.Models.DTOs;
+using LifeTrack.Core.Models.Entities;
+
+namespace LifeTrack.Core.Interfaces.Services.Mappers;
+
+public interface IUserMapper
+{
+    UserDTO Map (UserEntity entity);
+}
