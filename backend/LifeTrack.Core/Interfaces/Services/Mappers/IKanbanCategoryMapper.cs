be27a1ebf5e24b5fb9e51df4fcfda7a8096af07b
artemkiruhin <temkaunik@gmail.com>
2025-06-29 21:12:35 +0300

    add: kanban category mapper interface

---
+++ backend/LifeTrack.Core/Interfaces/Services/Mappers/IKanbanCategoryMapper.cs
@@ -0,0 +1,9 @@
+﻿using LifeTrack.Core.Models.DTOs;
+using LifeTrack.Core.Models.Entities;
+
+namespace LifeTrack.Core.Interfaces.Services.Mappers;
+
+public interface IKanbanCategoryMapper
+{
+    KanbanCategoryDTO Map (KanbanTaskCategoryEntity entity);
+}
