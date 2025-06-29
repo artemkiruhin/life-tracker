0b515288065e0188ce85d52381b2dd4ba000e621
artemkiruhin <temkaunik@gmail.com>
2025-06-29 21:11:28 +0300

    add: repository base interface

---
+++ backend/LifeTrack.Core/Interfaces/Repositories/Base/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace LifeTrack.Core.Interfaces.Repositories.Base;
+
+public interface IRepository<TEntity> where TEntity : class
+{
+    Task<TEntity?> GetByIdAsync(Guid id, CancellationToken ct);
+    Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken ct);
+    Task<IEnumerable<TEntity>> FindRangeAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken ct);
+    Task<TEntity?> FindAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken ct);
+    Task<TEntity> AddAsync(TEntity entity, CancellationToken ct);
+    TEntity? Update(TEntity entity);
+    Task<TEntity> DeleteAsync(TEntity entity, CancellationToken ct);
+}
