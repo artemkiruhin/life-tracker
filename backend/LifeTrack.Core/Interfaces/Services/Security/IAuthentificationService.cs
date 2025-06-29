8892cf6e6dae800955f2d9bf4f0e70233ab7d0a9
artemkiruhin <temkaunik@gmail.com>
2025-06-29 21:13:16 +0300

    add: auth service interface

---
+++ backend/LifeTrack.Core/Interfaces/Services/Security/IAuthentificationService.cs
@@ -0,0 +1,7 @@
+﻿namespace LifeTrack.Core.Interfaces.Services.Security;
+
+public interface IAuthentificationService
+{
+    Task<string> Login(string username, string password, CancellationToken ct);
+    Task<Guid> Registration(string username, string password, CancellationToken ct);
+}
