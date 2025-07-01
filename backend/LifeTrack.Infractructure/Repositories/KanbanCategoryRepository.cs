52718669c60e5ae1f02a16608df276d1d9ca5ea6
artemkiruhin <temkaunik@gmail.com>
2025-07-01 21:31:44 +0300

    add: kanban category repo impl

---
+++ backend/LifeTrack.Infractructure/Repositories/KanbanCategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using LifeTrack.Core.Interfaces.Repositories;
+using LifeTrack.Core.Models.Entities;
+using LifeTrack.Infractructure.Repositories.Base;
+
+namespace LifeTrack.Infractructure.Repositories;
+
+public class KanbanCategoryRepository(AppDbContext context) : BaseRepository<KanbanTaskCategoryEntity>(context), IKanbanCategoryRepository
+{
+    
+}
