30e8888fa8bcc99a540dce1f37ae068faf118c95
artemkiruhin <temkaunik@gmail.com>
2025-07-01 21:31:59 +0300

    add: user repo impl

---
+++ backend/LifeTrack.Infractructure/Repositories/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using LifeTrack.Core.Interfaces.Repositories;
+using LifeTrack.Core.Models.Entities;
+using LifeTrack.Infractructure.Repositories.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace LifeTrack.Infractructure.Repositories;
+
+public class UserRepository(AppDbContext context) : BaseRepository<UserEntity>(context), IUserRepository
+{
+    public async Task<UserEntity?> GetByEmailAsync(string email, CancellationToken ct)
+    {
+        return await DbSet.AsNoTracking().Where(user => user.Email == email).FirstOrDefaultAsync(ct);
+    }
+
+    public async Task<UserEntity?> GetByUsernameAsync(string username, CancellationToken ct)
+    {
+        return await DbSet.AsNoTracking().Where(user => user.Username == username).FirstOrDefaultAsync(ct);
+    }
+}
