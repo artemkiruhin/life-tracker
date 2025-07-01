f5a575ea87ed9c4a99b61a6d4bf3fa2026cdcb70
artemkiruhin <temkaunik@gmail.com>
2025-07-01 21:31:52 +0300

    add: kanban task repo impl

---
+++ backend/LifeTrack.Infractructure/Repositories/KanbanTaskRepository.cs
@@ -0,0 +1,10 @@
+﻿using LifeTrack.Core.Interfaces.Repositories;
+using LifeTrack.Core.Models.Entities;
+using LifeTrack.Infractructure.Repositories.Base;
+
+namespace LifeTrack.Infractructure.Repositories;
+
+public class KanbanTaskRepository(AppDbContext context) : BaseRepository<KanbanTaskEntity>(context), IKanbanTaskRepository
+{
+    
+}
