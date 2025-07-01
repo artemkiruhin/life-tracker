68feded43d965729dfe5e612488e88bf4ea93fff
artemkiruhin <temkaunik@gmail.com>
2025-07-01 21:32:10 +0300

    add: UoW interface

---
+++ backend/LifeTrack.Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using LifeTrack.Core.Interfaces.Repositories;
+
+namespace LifeTrack.Core.Interfaces;
+
+public interface IUnitOfWork
+{
+    Task<int> SaveChangesAsync(CancellationToken ct);
+    Task BeginTransactionAsync(CancellationToken ct);
+    Task CommitTransactionAsync(CancellationToken ct);
+    Task RollbackTransactionAsync(CancellationToken ct);
+    
+    public IUserRepository UserRepository { get; }
+    public IKanbanTaskRepository KanbanTaskRepository { get; }
+    public IKanbanCategoryRepository KanbanCategoryRepository { get; }
+}
