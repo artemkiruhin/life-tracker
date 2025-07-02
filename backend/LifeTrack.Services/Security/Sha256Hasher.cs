4336e780c0c979c5057975772dfda9b74374bbee
artemkiruhin <temkaunik@gmail.com>
2025-07-02 20:09:47 +0300

    add: sha 256 hasher implementation

---
+++ backend/LifeTrack.Services/Security/Sha256Hasher.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using LifeTrack.Core.Interfaces.Services.Security;
+
+namespace LifeTrack.Services.Security;
+
+public class Sha256Hasher : IHashingService
+{
+    public string HashData(string message) =>
+        Convert.ToHexStringLower(SHA256.HashData(Encoding.UTF8.GetBytes(message)));
+}
