9b649590f0b0cd39fafea794827ec2754f68430d
artemkiruhin <temkaunik@gmail.com>
2025-07-02 20:10:18 +0300

    add: jwt service implementation

---
+++ backend/LifeTrack.Services/Security/JwtService.cs
@@ -0,0 +1,47 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using LifeTrack.Core.Interfaces.Services.Security;
+using LifeTrack.Core.Models.Contracts;
+using Microsoft.IdentityModel.Tokens;
+
+namespace LifeTrack.Services.Security;
+
+public class JwtService : IJwtService
+{
+    private readonly JwtSettings _settings;
+
+    public JwtService(JwtSettings settings)
+    {
+        _settings = settings;
+    }
+    
+    public Result<string> GenerateToken(Guid userId)
+    {
+        try
+        {
+            var claims = new Claim[]
+            {
+                new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new(ClaimTypes.NameIdentifier, userId.ToString())
+            };
+            
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_settings.SecurityKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddHours(_settings.ExpiresInHours),
+                signingCredentials: creds,
+                issuer: _settings.Issuer,
+                audience: _settings.Audience
+            );
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+            return Result<string>.Success(tokenString);
+        }
+        catch (Exception e)
+        {
+            return Result<string>.Failure(e.Message);
+        }
+    }
+}
