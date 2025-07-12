cdb60c748b2fcaa5e1c313c254527bc36225efa9
artemkiruhin <temkaunik@gmail.com>
2025-07-12 22:43:29 +0300

    add: auth middleware

--- backend/LifeTrack.Api/Program.cs
+++ backend/LifeTrack.Api/Program.cs
@@ -1,3 +1,6 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using LifeTrack.Core.Interfaces;
 using LifeTrack.Core.Interfaces.Repositories;
 using LifeTrack.Core.Interfaces.Services.Entity;
@@ -9,7 +12,9 @@ using LifeTrack.Infractructure.Repositories;
 using LifeTrack.Services.Entity;
 using LifeTrack.Services.Mapper;
 using LifeTrack.Services.Security;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration =  builder.Configuration;
@@ -17,6 +22,66 @@ var configuration =  builder.Configuration;
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
+builder.Services.AddAuthentication(opt =>
+    {
+        opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(opt =>
+    {
+        opt.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = configuration["Jwt:Issuer"],
+            ValidAudience = configuration["Jwt:Issuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:SecurityKey"] ??
+                                                                               throw new ApplicationException(
+                                                                                   "SecurityKey is missing.")))
+        };
+        opt.Events = new JwtBearerEvents()
+        {
+            OnMessageReceived = context =>
+            {
+                var jwt = context.Request.Cookies["jwt"];
+                if (!string.IsNullOrEmpty(jwt)) context.Token = jwt;
+                return Task.CompletedTask;
+            },
+            OnTokenValidated = context =>
+            {
+                try
+                {
+                    var claims = context.Principal.Claims;
+                    var userIdClaim = claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
+                    if (Guid.TryParse(userIdClaim?.Value, out Guid userId))
+                    {
+                        var identity = context.Principal.Identity as ClaimsIdentity;
+                        if (identity != null)
+                        {
+                            if (!identity.HasClaim(c => c.Type == ClaimTypes.NameIdentifier))
+                            {
+                                identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, userId.ToString()));
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("--------ERROR--------");
+                        Console.WriteLine("Failed auth\n\n\n");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    context.Fail("Invalid token");
+                }
+
+                return Task.CompletedTask;
+            }
+        };
+    });
 
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
